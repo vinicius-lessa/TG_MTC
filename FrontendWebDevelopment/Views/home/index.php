e5d72b7ae65733e00768813675d25bc8ab07ceb9
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 20:31:41 -0300

    home

--- FrontendWebDevelopment/Views/home/index.php
+++ FrontendWebDevelopment/Views/home/index.php
@@ -25,6 +25,7 @@
     <link href="<?php echo SITE_URL ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo SITE_URL ?>/css/style.css">
     <link rel="icon" type="image/x-icon" href="<?php echo SITE_URL ?>/images/favicon.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <title> 
       <?php echo $titlePage; ?>
