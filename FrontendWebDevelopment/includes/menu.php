1b517218195c6068a474b8e6cfc8010e8e80adb6
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 15:53:19 -0300

    alteração header

--- FrontendWebDevelopment/includes/menu.php
+++ FrontendWebDevelopment/includes/menu.php
@@ -2,26 +2,34 @@
   <!-- Fixed navbar -->
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="<?php echo SITE_URL ?>/Views/home/index.php">MTC - Music Trade Center</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="row align-items-start navbar-brand" href="<?php echo SITE_URL ?>/Views/home/index.php">
+      <div class="col-4">
+      <img src="images/ícone.png" class="img-thumbnail" alt="...">
+      </a>
+      </div>
+      <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+      </button> -->
+      <div class="col-4">
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo SITE_URL ?>/Views/Users/Cadastro.php">Cadastrar-se</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">Login</a>
-          </li>
         </ul>
-        <form class="d-flex">
+      </div>
+        <!-- <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </form> -->
+        <div class="col-4">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo SITE_URL ?>/Views/Users/Cadastro.php">Cadastrar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Entrar</a>
+          </li>
+        </div>
       </div>
     </div>
   </nav>
