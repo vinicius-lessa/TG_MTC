0f340b8fd2ea42cf351ab29c67b3f7e3aa8e52f3
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 16:28:54 -0300

    header 19

--- FrontendWebDevelopment/includes/menu.php
+++ FrontendWebDevelopment/includes/menu.php
@@ -3,15 +3,24 @@
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bk-preto">
     <div class="container-fluid">
       <a class="navbar-brand" href="<?php echo SITE_URL ?>/Views/home/index.php">
-      <img src="css/images/favicon.ico" alt="" width="30" height="24">
+      <img src="../css/images/favicon.ico" alt="" width="30" height="24">
       </a>
       <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button> -->
       <!-- <div class="collapse navbar-collapse" id="navbarCollapse"> -->
         <ul class="nav justify-content-center">
-          <li class="nav-item">
+        <li class="nav-item">
             <a class="nav-link text-white" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="#">Anúncios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="#">Feed Musical</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="#">Music Trade Center</a>
           </li>
         </ul>
       <!-- </div> -->
