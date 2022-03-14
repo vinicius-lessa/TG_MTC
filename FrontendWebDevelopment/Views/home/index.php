33a1b0b2d9a4f08dfaf40f3b432e1335cb117437
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 20:19:53 -0300

    home 10

--- FrontendWebDevelopment/Views/home/index.php
+++ FrontendWebDevelopment/Views/home/index.php
@@ -57,7 +57,19 @@
     </main>
     
     <!-- MEIO DA HOME -->
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+
+  <div class="col bk-left">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-red" style="text-align:center;"><strong>25</strong></h5>
+        <p class="card-title text-white" style="text-align:center;">Usuários em todo o</p>
+        <p class="card-title text-white" style="text-align:center;">Brasil.</p>
+      </div>
+    </div>
+  </div>
+
   <div class="col">
     <div class="card">
       <img src="..." class="card-img-top" alt="...">
@@ -67,15 +79,7 @@
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
+
   <div class="col">
     <div class="card">
       <img src="..." class="card-img-top" alt="...">
@@ -85,15 +89,17 @@
       </div>
     </div>
   </div>
+
   <div class="col">
     <div class="card">
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
   </div>
+
 </div>
 
     <div class="container">
@@ -104,19 +110,6 @@
         <h5 class="text-white" style="text-align:center;"><strong>Usuários em todo o</strong></h5>
         <h5 class="text-white" style="text-align:center;"><strong>Brasil</strong></h5>
         </div>
-
-        <div class="col-4">
-        <h1 class="text-red" style="text-align:center;"><strong>25</strong></h1>
-        <h5 class="text-white" style="text-align:center;"><strong>Usuários em todo o</strong></h5>
-        <h5 class="text-white" style="text-align:center;"><strong>Brasil</strong></h5>
-        </div>
-
-        <div class="col-4">
-        <h1 class="text-red" style="text-align:center;"><strong>25</strong></h1>
-        <h5 class="text-white" style="text-align:center;"><strong>Usuários em todo o</strong></h5>
-        <h5 class="text-white" style="text-align:center;"><strong>Brasil</strong></h5>
-        </div>
-        
       </div>
     </div>
 
