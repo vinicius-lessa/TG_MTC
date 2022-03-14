6b363b9afa033a7976ce9fb2eb79ba78799cae17
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 16:10:53 -0300

    header 08

--- FrontendWebDevelopment/includes/menu.php
+++ FrontendWebDevelopment/includes/menu.php
@@ -8,13 +8,13 @@
       <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button> -->
-      <div class="collapse navbar-collapse" id="navbarCollapse">
+      <!-- <div class="collapse navbar-collapse" id="navbarCollapse"> -->
         <ul class="nav justify-content-center">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
         </ul>
-      </div>
+      <!-- </div> -->
         <!-- <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
