cc07c22d9adef7f02cc2103e512c256409036eec
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 17:45:20 -0300

    header 41

--- FrontendWebDevelopment/Views/home/index.php
+++ FrontendWebDevelopment/Views/home/index.php
@@ -47,7 +47,7 @@
           </div>
 
           <div class="col-6 mt-5">
-            <h6 class="text-white">Negociação Online</h6>
+            <h6 class="text-red"><b>Negociação Online</b></h6>
             <h1 class="text-white">Music Trade Center</h1>
             <p class="text-white">Aqui você faz negociação online diretamente com o vendedor, de forma transparente e segura, a qualquer horário, plataforma totalmente dedicadas a você.</p>
             <button type="button" class="btn btn-danger text-white"><a href="#">NEGOCIE AGORA</a></button>
