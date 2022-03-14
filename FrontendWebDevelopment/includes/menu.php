3cb56d5a62ee5847f064151e1a40d9fab66dca02
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 16:33:32 -0300

    header 23

--- FrontendWebDevelopment/includes/menu.php
+++ FrontendWebDevelopment/includes/menu.php
@@ -3,13 +3,13 @@
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bk-preto">
     <div class="container-fluid">
       <a class="navbar-brand" href="<?php echo SITE_URL ?>/Views/home/index.php">
-      <img src="/FrontendWebDevelopment/images/favicon.ico" alt="" width="30" height="24">
+      <img src="FrontendWebDevelopment/images/favicon.ico" alt="" width="30" height="24">
       </a>
       <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button> -->
       <!-- <div class="collapse navbar-collapse" id="navbarCollapse"> -->
-        <ul class="nav justify-content-center ml-5">
+        <ul class="nav justify-content-center">
         <li class="nav-item">
             <a class="nav-link text-white" aria-current="page" href="#">Home</a>
           </li>
@@ -23,12 +23,7 @@
             <a class="nav-link text-white" aria-current="page" href="#">Music Trade Center</a>
           </li>
         </ul>
-      <!-- </div> -->
-        <!-- <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form> -->
-      <!-- <div class="collapse navbar-collapse" id="navbarCollapse"> -->
+
       <ul class="nav justify-content-end">
         <li class="nav-item">
             <a class="nav-link text-white" href="<?php echo SITE_URL ?>/Views/Users/Cadastro.php">Cadastrar</a>
@@ -37,17 +32,16 @@
             <a class="nav-link text-white" href="<?php echo SITE_URL ?>/Views/Users/Cadastro.php">Entrar</a>
         </li>
       </ul>
-      <!-- </div> -->
 
     </div>
   </nav>
 </header>
 
 <!-- Logomarca -->
-<!-- <div class="row">
+<div class="row">
   <div class="col-md-12">
     <a href="<?php echo SITE_URL ?>/Views/home/index.php"><img id="logo-header" alt="Logo do site"/>
       MTC - Music Trade Center
     <a>
   </div>
-</div> -->
+</div>
