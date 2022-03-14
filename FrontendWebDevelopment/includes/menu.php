4f93d731ec5412238bc0b8f8f9a8a3465f476814
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 16:03:51 -0300

    header 04

--- FrontendWebDevelopment/includes/menu.php
+++ FrontendWebDevelopment/includes/menu.php
@@ -3,7 +3,7 @@
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bk-preto">
     <div class="container-fluid">
       <a class="row align-items-start navbar-brand" href="<?php echo SITE_URL ?>/Views/home/index.php">
-      <img src="images/favicon.ico" class="img-fluid" alt="...">
+      <img src="images/favicon.ico" class="rounded float-start" alt="Ícone">
       </a>
       <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -14,6 +14,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
         </ul>
+      </div>
         <!-- <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
@@ -23,7 +24,7 @@
           </li>
           <li class="nav-item">
             <a class="nav-link">Entrar</a>
-          </li>
+        </li>
 
     </div>
   </nav>
