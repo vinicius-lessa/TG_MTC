f01f5468735bc4338d0f78e0a805c8d30b6bbf25
renatacarrillo <renata_carrillo02@hotmail.com>
2022-03-14 21:16:35 -0300

    home icons 24

--- FrontendWebDevelopment/Views/home/index.php
+++ FrontendWebDevelopment/Views/home/index.php
@@ -24,7 +24,7 @@
         
     <link href="<?php echo SITE_URL ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo SITE_URL ?>/css/style.css">
-    <link rel="icon" type="image/x-icon" href="<?php echo SITE_URL ?>/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="<?php echo SITE_URL ?>/images/icon.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <title> 
