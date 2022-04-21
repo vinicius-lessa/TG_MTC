a3efe544663bcf92b8241a4e45d4787e8e6ac0f1
vinicius-lessa <vinicius.lessa33@outlook.com>
2022-04-21 20:00:02 -0300

    avanços no chat

--- FrontendWebDevelopment/Views/users/chat.php
+++ FrontendWebDevelopment/Views/users/chat.php
@@ -98,7 +98,7 @@ endif;
 
         <!-- Chat Boxes -->
         <div class="container text-white">
-          <div class="row" style="height: 500px; background-color: rgba(255,0,0,0.1);">
+          <div class="row chat_panel">
             <!-- Current Chat -->
             <div class="col-12 col-sm-8 p-2">
               <div class="shadow bk-gray p-3 border border-danger rounded h-100">
@@ -111,11 +111,10 @@ endif;
                 <hr class="mt-1"> -->
 
                 <div class="row px-2 h-100">
-                  <div class="col-12 bk-chat h-100 rounded p-3 d-flex reverse-direction">
+                  <div class="col-12 bk-chat h-100 rounded pt-2 pb-2 d-flex reverse-vertical-direction">
 
                     <!-- Input Mensage -->                    
-                    <div class="bk-orange" style="height: 45px;">
-                      <!-- <h3>Teste</h3> -->
+                    <div class="mx-0" style="height: 45px;">                      
                       <form>
                         <div class="d-flex">
                           <!-- <div class="bk-white"> -->
@@ -129,53 +128,98 @@ endif;
                     </div>
                     
                     <!-- ScrollBar -->
-                    <div class="bk-purple" style="height: 200px;">
-                      <div class="scrollbar" id="chat">
+                    <div class="row mx-0 p-0" style="height: 380px;">
+                      <div class="col-12 d-flex scrollbar h-100 reverse-vertical-direction px-0" id="chat">
                         
-                        <div class="col-12 mb-1">
-                          <small>Olá Fulano, tudo bem?</small>
-                        </div>
-                        <div class="col-12 mb-4">
-                          <div class="col-12">
-                            <strong><span>Rafaela Alle</span></strong>
+                        <div class="d-flex flex-row">
+                          <div class="my-1 bk-lightgray rounded msg-width">
+                            <div class="m-0">
+                              <div class="col-12 mt-1 p-0 px-2">
+                                <strong><span>Rafaela Alle</span></strong>
+                              </div>
+                              <div class="col-12 mb-2 p-0 px-2">
+                                <div class="m-0 p-0">
+                                  <small>Olá Vinícius, tudo bem?</small>
+                                </div>                                
+                              </div>
+                            </div>
                           </div>
                         </div>
 
-                        <div class="col-12 mb-1">
-                          <small>Olá Fulano, tudo bem?</small>
-                        </div>
-                        <div class="col-12 mb-4">
-                          <div class="col-12">
-                            <strong><span>Rafaela Alle</span></strong>
+                        <div class="d-flex flex-row-reverse">
+                          <div class="my-1 bk-lightgray rounded msg-width">
+                            <div class="m-0">
+                              <div class="col-12 mt-1 d-flex flex-row-reverse p-0 px-2">
+                                <strong><span>Você</span></strong>
+                              </div>
+                              <div class="col-12 mb-2 d-flex flex-row-reverse p-0 px-2">
+                                <div class="m-0 p-0">                                                            
+                                  <small>Olá, tudo sim, e voce?</small>
+                                </div>
+                              </div>
+                            </div>
                           </div>
                         </div>
 
-                        <div class="col-12 mb-1">
-                          <small>Olá Fulano, tudo bem?</small>
-                        </div>
-                        <div class="col-12 mb-4">
-                          <div class="col-12">
-                            <strong><span>Rafaela Alle</span></strong>
-                          </div>
-                        </div>
-
-                        <div class="col-12 mb-1">
-                          <small>Olá Fulano, tudo bem?</small>
-                        </div>
-                        <div class="col-12 mb-4">
-                          <div class="col-12">
-                            <strong><span>Rafaela Alle</span></strong>
+                        <div class="d-flex flex-row">
+                          <div class="my-1 bk-lightgray rounded msg-width">
+                            <div class="m-0">
+                              <div class="col-12 mt-1 p-0 px-2">
+                                <strong><span>Rafaela Alle</span></strong>
+                              </div>
+                              <div class="col-12 mb-2 p-0 pl-2">
+                                <div class="m-0 p-0">
+                                  <small>Estou bem, estou interessando no seu equipamento, ele possui algum defeito?</small>
+                                </div>
+                              </div>
+                            </div>
                           </div>
                         </div>
                         
-                        <div class="col-12 mb-1">
-                          <small>Olá Fulano, tudo bem?</small>
-                        </div>
-                        <div class="col-12 mb-4">
-                          <div class="col-12">
-                            <strong><span>Rafaela Alle</span></strong>
+                        <div class="d-flex flex-row-reverse">
+                          <div class="my-1 bk-lightgray rounded msg-width">
+                            <div class="m-0">
+                              <div class="col-12 mt-1 d-flex flex-row-reverse p-0 px-2">
+                                <strong><span>Você</span></strong>
+                              </div>
+                              <div class="col-12 mb-2 d-flex flex-row-reverse p-0 px-2">
+                                <div class="m-0 p-0">
+                                  <small>Legal, não possui, tenho ele há 4 anos, nunca me deu problemas.</small>
+                                </div>
+                              </div>
+                            </div>
                           </div>
                         </div>
+                        
+                        <div class="d-flex flex-row">
+                          <div class="my-1 bk-lightgray rounded msg-width">
+                            <div class="m-0">
+                              <div class="col-12 mt-1 p-0 px-2">
+                                <strong><span>Rafaela Alle</span></strong>
+                              </div>
+                              <div class="col-12 mb-2 p-0 px-2">
+                                <div class="m-0 p-0">
+                                  <small>Ótimo! Onde e quando posso te encontrar?</small>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        
+                        <div class="d-flex flex-row-reverse">
+                          <div class="my-1 bk-lightgray rounded msg-width">
+                            <div class="m-0">
+                              <div class="col-12 mt-1 d-flex flex-row-reverse p-0 px-2">
+                                <strong><span>Você</span></strong>
+                              </div>
+                              <div class="col-12 mb-2 d-flex flex-row-reverse p-0 px-2">
+                                <div class="m-0 p-0">
+                                  <small>Aqui na rua xxx numero xxx, São Roque!</small>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>                      
                         
                       </div>  
                     </div>
@@ -187,16 +231,16 @@ endif;
             <!-- Other Chats -->
             <div class="col-12 col-sm-4 p-2">
               <div class="shadow bk-gray p-3 rounded h-100">
-                <div class="row">
+                <div class="row text-center">
                   <div class="col-12">
-                    <strong><h5>Outras Conversas</h5></strong>
+                    <strong><h5>OUTRAS CONVERSAS</h5></strong>
                   </div>                  
                 </div>
 
                 <hr class="mt-1">
 
-                <div class="row p-2" style="height: 300px;">
-                  <div class="scrollbar h-100">
+                <div class="row p-2" style="height: 400px;">
+                  <div class="scrollbar h-100 w-100">
                     <div class="col-12">
                       <div class="row mb-1">
                         <strong><span>Rafaela Alle</span></strong>
