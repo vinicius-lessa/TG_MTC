1053c896fb8193adc6e451c48524fc3d262cdf87
renatacarrillo <renata_carrillo02@hotmail.com>
2022-04-23 21:11:37 -0300

    mudanças (inacabadas) trade_post_detailed

--- FrontendWebDevelopment/Views/trade_posts/trade_post_detailed.php
+++ FrontendWebDevelopment/Views/trade_posts/trade_post_detailed.php
@@ -86,49 +86,26 @@ $userCreator = $tpDetails["data"][0]["user_id"];
             <div class="col-12">
               <hr class="hr-default">
             </div>
-          </div>        
-          
-          <!-- Trade Post Details -->
-          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <!-- <div class="dpotTF"></div> -->
-            <div class="carousel-inner bQbWAr">
-              <div class="d-flex justify-content-center carousel-item active">
-                <img class="image" src="<?php echo $tpDetails["data"][0]["image_name"] ?>" alt="First slide" style="max-width:100%;height:400px;">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo $tpDetails["data"][0]["image_name"] ?>" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo $tpDetails["data"][0]["image_name"] ?>" alt="Third slide">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
-          <!-- <div class="row">
-            <div class="col-8 col-sm-8">
-              <a href="#">
-                <img src="<?php //echo $tpDetails["data"][0]["image_name"] ?>" class="img-fluid" alt="" style="max-width:100%;width:695px;height:595px;object-fit:cover;">
-              </a>
+                  
+          <!-- IMAGENS -->
+          <div class="container">
+            <div class="row">
+              <div class="col-8 bQbWAr">
+                <img class="image" src="<?php echo $tpDetails["data"][0]["image_name"] ?>">
+              </div>
+
+              <div class="col-4 bk-green">
+                <div class="col-6 mb-3">
+                  <img class="img-fluid" src="<?php echo $tpDetails["data"][0]["image_name"] ?>">
+                </div>
+                <div class="col-6">
+                  <img class="img-fluid" src="<?php echo SITE_URL ?>/images/icon.png">
+                </div>
+              </div>
             </div>
-            
-            <div class="col-4 col-sm-4">
-                <img src="<?php //echo $tpDetails["data"][0]["image_name"] ?>" class="img-fluid" alt="" style="max-width:100%;width:395px;height:295px;object-fit:cover;">
-                <img src="<?php //echo $tpDetails["data"][0]["image_name"] ?>" class="img-fluid mt-3" alt="" style="max-width:100%;width:395px;height:295px;object-fit:cover;">
-            </div>
-          </div> -->
-                    
+          </div>
+
           <!-- Principais Informações -->
           <div class="row mt-4">
             <div class="col-12 col-sm-8">
