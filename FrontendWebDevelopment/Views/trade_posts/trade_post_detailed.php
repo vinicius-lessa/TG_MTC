4df61b281dbdf30e6a8e7522c34f53d239a51e9a
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2022-05-03 11:36:38 -0300

    carousel

--- FrontendWebDevelopment/Views/trade_posts/trade_post_detailed.php
+++ FrontendWebDevelopment/Views/trade_posts/trade_post_detailed.php
@@ -85,7 +85,43 @@ $isOwnPost    = $tpDetails["data"][0]["user_id"] === $_SESSION['user_id'];
               <hr class="hr-default">
             </div>
           </div>
-                  
+               
+          
+<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo SITE_URL ?>/images/icons/default-profile-img.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo $tpDetails["data"][0]["image_name"] ?>" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo SITE_URL ?>/images/icons/default-profile-img.png" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> -->
+
+<input type="radio" name="position" checked />
+  <input type="radio" name="position" />
+  <input type="radio" name="position" />
+  <input type="radio" name="position" />
+  <input type="radio" name="position" />
+  <main id="carousel">
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+  </main>
+
           <!-- Main Content -->          
           <article>              
             <div class="row">
